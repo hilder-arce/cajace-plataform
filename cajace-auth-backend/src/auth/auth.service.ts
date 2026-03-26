e1136b1c975de0e38b604c9c2d6fc1662fc57409
hilder-arce <hilderarce566@gmail.com>
2026-03-26 13:07:18 -0500

    Changes for production

--- cajace-auth-backend/src/auth/auth.service.ts
+++ cajace-auth-backend/src/auth/auth.service.ts
@@ -471,15 +471,15 @@ export class AuthService {
     
     res.cookie('access_token', accessToken, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 15 * 60 * 1000,
     });
     
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
